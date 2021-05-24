16c9a983dbdbe995a01c68b4654fa94416d32d0e
최혜정 <hjchoi2000@seesunit.com>
2021-05-24 13:48:10 +0900

    1일 1커밋 다시 시작  java 복습 변수와 자료형(1) 완료  날짜 잘 못 쓴거 수정...

---
+++ JAVA/src/first/BinaryTest2.java
@@ -0,0 +1,13 @@
+package binary;
+
+public class BinaryTest2 {
+    public static void main(String[] args){
+        int num1 = 00000000000000000000000000101;
+        int num2 = 00111111111111111111111111010;
+
+        int sum = num1 + num2;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(sum);
+    }
+}
