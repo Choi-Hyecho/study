223a90be2147de30221f4e2839477317aea0f24a
hjchoi <hyecho.choi@gmail.com>
2021-05-25 13:32:24 +0900

    03. 변수와 자료형(2) 진행 중

---
+++ JAVA/src/second/VariableEx.java
@@ -0,0 +1,18 @@
+public class VariableEx {
+
+    public static void main(String[] args) {
+        
+        // 변수 선언
+        int num;
+        // 오른쪽에 있는 값을 왼쪽에 대입
+        num = 10;
+
+        //선언한 변수 출력
+        System.out.println(num);
+
+        // 선언한 변수에 값을 넣는 것을 초기화라고 함.
+        // 변수 선언과 동시에 초기화 할 수 있음.
+        let level = 20;
+        System.out.println(level);
+    }
+}
