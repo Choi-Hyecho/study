92661a44b160a94169fcdc0e339f9aeb0659e75f
hjchoi <hyecho.choi@gmail.com>
2021-05-31 18:20:22 +0900

    옮기기 커밋

---
+++ JAVA/src/ch03/OperationEx4.java
@@ -0,0 +1,16 @@
+public class OperationEx4 {
+    public static void main(String[] args) {
+        int num1 =10;
+        // 대입연산자가 아니기 때문에 값이 변경됨.
+
+        System.out.println(num1 += 1);
+        System.out.println(num1 %= 10);
+        num1 -= 1 // num1 = num1 -1
+        System.out.println(num);
+
+
+        // 첫 항에 있는 조건이 참일때 왼쪽항, 거짓일 때 오른쪽 항 값이 들어감.
+        int num = (5>3)? 10: 20 ;
+        System.out.println(num);
+    }
+}
